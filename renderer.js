11aafc938117d2889cd15746e9b296850eeb9db4
Rochelle Burrows <rochelleburrows@Rochelles-MacBook-Pro.local>
2019-05-24 18:50:09 +0100

    replaced placeholders with actual values

--- renderer.js
+++ renderer.js
@@ -1,10 +1,20 @@
 const fs = require("fs");
 
+const mergeValues = (values, content) => {
+  // Cycle over the keys
+  for(let key in values){
+        //Replace all {{key}} with the values from the values object
+        content = content.replace("{{" + key + "}}", values[key]);
+  }
+  //return merged content
+  return content;
+};
+
 const view = (templateName, values, response) => {
   //Read from the template filesa
-  const fileContents = fs.readFileSync("./views/" + templateName + ".html");
+  let fileContents = fs.readFileSync("./views/" + templateName + ".html", {encoding: "utf8"});
   // Insert values into the Content
-  
+  fileContents = mergeValues(values, fileContents);
   //Write out the response
   response.write(fileContents);
 };
